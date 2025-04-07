9fde78b00d2b934a91f93f6b6c2e4288eeb68d9c
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-04-07 11:07:21 +0200

    Added: testimonial fields.

--- src/Admin/Downloads/Editor/Branding.php
+++ src/Admin/Downloads/Editor/Branding.php
@@ -8,17 +8,25 @@ namespace Daan\Branding\Admin\Downloads\Editor;
 use EDD\Admin\Downloads\Editor\Section;
 
 class Branding extends Section {
-	const BRAND       = '_edd_featured_download_brand';
+	const BRAND                    = '_edd_featured_download_brand';
 
-	const LOGO_NORMAL = '_edd_featured_download_logo_normal';
+	const LOGO_NORMAL              = '_edd_featured_download_logo_normal';
 
-	const LOGO_LIGHT  = '_edd_featured_download_logo_light';
+	const LOGO_LIGHT               = '_edd_featured_download_logo_light';
 
-	const ICON        = '_edd_featured_download_icon';
+	const ICON                     = '_edd_featured_download_icon';
 
-	const TAGLINE     = '_edd_featured_download_tagline';
+	const TAGLINE                  = '_edd_featured_download_tagline';
 
-	const BUTTON_TEXT = '_edd_featured_download_button_text';
+	const BUTTON_TEXT              = '_edd_featured_download_button_text';
+
+	const TESTIMONIAL_TITLE        = '_edd_featured_download_testimonial_title';
+
+	const TESTIMONIAL              = '_edd_featured_download_testimonial';
+
+	const TESTIMONIAL_AUTHOR       = '_edd_featured_download_testimonial_author';
+
+	const TESTIMONIAL_AUTHOR_IMAGE = '_edd_featured_download_testimonial_author_image';
 
 	/**
 	 * Section ID.
@@ -164,6 +172,60 @@ class Branding extends Section {
 					]
 				);
 				$copy->output();
+				?>
+            </div>
+            <div class="edd-form-group__control">
+				<?php
+				$copy = new \EDD\HTML\Text(
+					[
+						'label' => __( 'Testimonial Title', 'daan-featured-downloads-branding' ),
+						'id'    => self::TESTIMONIAL_TITLE,
+						'name'  => self::TESTIMONIAL_TITLE,
+						'value' => get_post_meta( $download->ID, self::TESTIMONIAL_TITLE, true ),
+					]
+				);
+				$copy->output();
+				?>
+            </div>
+            <div class="edd-form-group__control">
+				<?php
+				$copy = new \EDD\HTML\Textarea(
+					[
+						'label' => __( 'Testimonial Content', 'daan-featured-downloads-branding' ),
+						'id'    => self::TESTIMONIAL,
+						'name'  => self::TESTIMONIAL,
+						'value' => get_post_meta( $download->ID, self::TESTIMONIAL, true ),
+					]
+				);
+				$copy->output();
+				?>
+            </div>
+            <div class="edd-form-group__control">
+				<?php
+				$copy = new \EDD\HTML\Text(
+					[
+						'label' => __( 'Testimonial Author', 'daan-featured-downloads-branding' ),
+						'id'    => self::TESTIMONIAL_AUTHOR,
+						'name'  => self::TESTIMONIAL_AUTHOR,
+						'value' => get_post_meta( $download->ID, self::TESTIMONIAL_AUTHOR, true ),
+					]
+				);
+				$copy->output();
+				?>
+            </div>
+            <label for="<?php echo self::TESTIMONIAL_AUTHOR_IMAGE; ?>" class="edd-form-group__label">
+				<?php echo esc_html( __( 'Author Image:', 'daan-featured-downloads-branding' ) ); ?>
+            </label>
+            <div class="edd-form-group__control">
+				<?php
+				$logo = new \EDD\HTML\Upload(
+					[
+						'id'    => self::TESTIMONIAL_AUTHOR_IMAGE,
+						'name'  => self::TESTIMONIAL_AUTHOR_IMAGE,
+						'value' => get_post_meta( $download->ID, self::TESTIMONIAL_AUTHOR_IMAGE, true ),
+					]
+				);
+				$logo->output();
 				?>
             </div>
         </div>
